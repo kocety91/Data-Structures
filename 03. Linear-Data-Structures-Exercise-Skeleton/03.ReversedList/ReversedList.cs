7d3a66331ae974c762d3d0ac51be74fe9459b57a
kocety91 <kocety91@gmail.com>
2022-04-07 15:58:58 +0300

    Implement reveresd list

--- 03. Linear-Data-Structures-Exercise-Skeleton/03.ReversedList/ReversedList.cs
+++ 03. Linear-Data-Structures-Exercise-Skeleton/03.ReversedList/ReversedList.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class ReversedList<T> : IAbstractList<T>
     {
@@ -25,11 +26,21 @@
         {
             get
             {
-                throw new NotImplementedException();
+                this.ValidateIndex(index);
+                return this.items[this.Count - 1 - index];
             }
             set
             {
-                throw new NotImplementedException();
+                this.ValidateIndex(index);
+                this.items[index] = value;
+            }
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || this.Count <= index)
+            {
+                throw new IndexOutOfRangeException();
             }
         }
 
@@ -37,42 +48,90 @@
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            if (this.Count == this.items.Length)
+            {
+                this.Grow();
+            }
+
+            this.items[this.Count++] = item;
+        }
+
+        private void Grow()
+        {
+            var newArray = new T[this.items.Length * 2];
+            Array.Copy(this.items, newArray, this.Count);
+            this.items = newArray;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            var number = this.IndexOf(item);
+
+            return number == -1 ? false : true;
         }
 
         public int IndexOf(T item)
         {
-            throw new NotImplementedException();
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                if (this.items[i].Equals(item))
+                {
+                    return this.Count - 1 - i;
+                }
+            }
+            return -1;
         }
 
         public void Insert(int index, T item)
         {
-            throw new NotImplementedException();
+            ValidateIndex(index);
+            Grow();
+
+            for (int i = this.Count; i > this.Count -1 -index; i--)
+            {
+                this.items[i + 1] = this.items[i];
+            }
+
+            this.items[this.Count - index] = item;
+            this.Count++;
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            var index = this.IndexOf(item);
+
+            if (index == -1)
+            {
+                return false;
+            }
+
+            this.RemoveAt(index);
+            return true;
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            this.ValidateIndex(index);
+
+            var currentPosition = this.Count - 1 - index;
+
+            for (int i = currentPosition; i <= this.Count - 1; i++)
+            {
+                this.items[i] = this.items[i + 1];
+            }
+
+            this.Count--;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                yield return this.items[i];
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        => this.GetEnumerator();
     }
 }
---
+++ 03. Linear-Data-Structures-Exercise-Skeleton/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using Problem03.ReversedList;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ReversedList<string>() { "koce", "misho", "neli" };
+
+            list.Insert(0, "anton");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
