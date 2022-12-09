596277538ed3c9f17363fbe16b404a5129ec6a46
kocety91 <kocety91@gmail.com>
2022-12-09 17:11:27 +0200

    Add as string method to my tree implementation

---
+++ Training/Trees-Representation/TrainingTree/IAbstractTree.cs
@@ -0,0 +1,22 @@
+﻿namespace TrainingTree
+{
+    public interface IAbstractTree<T>
+    {
+        T Key { get; }
+        IReadOnlyCollection<Tree<T>> Children { get; }
+
+        void AddParent(Tree<T> parent);
+
+        void AddChild(Tree<T> child);
+
+        string AsString();
+
+        IEnumerable<T> GetLeafKeys();
+
+        IEnumerable<T> GetInternalKeys();
+
+        T GetDeepestKey();
+
+        IEnumerable<T> GetLongestPath();
+    }
+}
---
+++ Training/Trees-Representation/TrainingTree/Tree.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+namespace TrainingTree
+{
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private List<Tree<T>> _children;
+
+        public Tree(T key)
+        {
+            Key = key;
+            _children = new List<Tree<T>>();
+        }
+
+        public Tree(T key, params Tree<T>[] children)
+            :this(key)
+        {
+            foreach (var child in children)
+            {
+                _children.Add(child);
+            }
+        }
+
+        public T Key { get; private set; }
+
+        public Tree<T> Parent { get; private set; }
+
+        public IReadOnlyCollection<Tree<T>> Children => _children.AsReadOnly();
+
+        public void AddChild(Tree<T> child)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddParent(Tree<T> parent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AsString()
+        {
+            var sb = new StringBuilder();
+            int counter = 0;
+            SetStringBuilder(this, sb,ref counter);
+            return sb.ToString();
+        }
+
+        public IEnumerable<T> GetInternalKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetLeafKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetDeepestKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetLongestPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SetStringBuilder(Tree<T> tree , StringBuilder sb,ref int counter)
+        {
+            sb.AppendLine(new string(' ',counter) + tree.Key.ToString());
+
+            foreach (var child in tree.Children)
+            {
+                counter += 2;
+                SetStringBuilder(child, sb,ref counter);
+            }
+            counter -= 2;
+        }
+    }
+}
