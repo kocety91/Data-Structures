40456f2bc4d414da85f655e2632524088664ca8c
kocety91 <kocety91@gmail.com>
2022-12-13 16:10:17 +0200

    Add BinarySearchTree

---
+++ 05.Binary-Search-Tree/MyBinarySearchTree/AbstractBinarySearchTree.cs
@@ -0,0 +1,36 @@
+﻿namespace MyBinarySearchTree
+{
+    public class AbstractBinarySearchTree<T> : IAbstractBinarySearchTree<T>
+        where T : IComparable<T>
+    {
+        private class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            public T Value { get; set; }
+
+            public Node Left { get; set; }
+
+            public Node Right { get; set; }
+        }
+
+        private Node _root;
+
+        public void EachInOrder(Action<T> action)
+        {
+            EachInOrderDfs(_root, action);
+        }
+
+        private void EachInOrderDfs(Node node, Action<T> action)
+        {
+            if (node == null) return;
+
+            EachInOrderDfs(node.Left, action);
+            action.Invoke(node.Value);
+            EachInOrderDfs(node.Right, action);
+        }
+    }
+}
---
+++ 05.Binary-Search-Tree/MyBinarySearchTree/IAbstractBinarySearchTree.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBinarySearchTree
+{
+    public interface IAbstractBinarySearchTree<T>
+        where T : IComparable<T>
+    {
+        void EachInOrder(Action<T> action);
+    }
+}
